67bdccad3b26a157f026d149562208c497ddb551
fbm <wuq_xd@163.com>
2020-09-24 20:36:02 +0800

    主应用 main.js

--- main/src/main.js
+++ main/src/main.js
@@ -10,17 +10,11 @@ Vue.config.productionTip = false
 
 registerMicroApps([
   {
-    name: 'micro app1', // app name registered
+    name: 'microApp1', // app name registered
     entry: '//localhost:8081',
     container: '#container',
     activeRule: '/app1',
   },
-  // {
-  //   name: 'vue app',
-  //   entry: { scripts: ['//localhost:7100/main.js'] },
-  //   container: '#yourContainer2',
-  //   activeRule: '/yourActiveRule2',
-  // },
 ]);
 
 start();
